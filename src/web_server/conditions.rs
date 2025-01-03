a33679241ad3a1fd24061beaaaa3158acc3c443a
Michal Kalita <michal.kalita@prusa3d.cz>
2025-01-03 16:09:37 +0100

    test conditions

--- src/web_server/conditions.rs
+++ src/web_server/conditions.rs
@@ -16,6 +16,9 @@ pub enum Condition {
     Hours(u32, u32),
     #[serde(rename = "percentile")]
     PercentileInRange { value: f32, range: Range },
+
+    #[cfg(test)]
+    Debug(bool),
 }
 
 pub trait Eval {
@@ -25,24 +28,196 @@ pub trait Eval {
 impl Eval for Condition {
     fn evaluate(&self, ctx: &EvaluateContext) -> bool {
         match self {
-            Condition::And(items) => items.iter().all(|i| i.evaluate(ctx)),
-            Condition::Or(items) => items.iter().any(|i| i.evaluate(ctx)),
+            Condition::And(items) if items.len() > 0 => items.iter().all(|i| i.evaluate(ctx)),
+            Condition::Or(items) if items.len() > 0 => items.iter().any(|i| i.evaluate(ctx)),
+            Condition::And(_) | Condition::Or(_) => false,
+
             Condition::Not(item) => !item.evaluate(ctx),
             Condition::PriceLowerThan(price) => ctx.prices.prices[ctx.prices.now_index] <= *price,
             Condition::Hours(min, max) => {
-                let hour = ctx.now.hour() + 1;
+                let hour = ctx.now.hour();
 
-                *min <= hour && hour < *max
+                *min <= hour && hour <= *max
             }
             Condition::PercentileInRange {
                 value: target_percentile,
                 range,
             } => {
                 let actual_percentile = ctx.limit(*range).percentile();
+                println!("actual percentile {}", actual_percentile);
 
-                actual_percentile >= *target_percentile
+                actual_percentile <= *target_percentile
             }
+
+            #[cfg(test)]
+            Condition::Debug(state) => *state,
         }
+    }
+}
+
+#[cfg(test)]
+mod condition_tests {
+    use chrono::NaiveDateTime;
+
+    use crate::web_server::conditions::Eval;
+
+    use super::{Condition, EvaluateContext, Range};
+
+    fn setup() -> EvaluateContext {
+        EvaluateContext::new(
+            NaiveDateTime::parse_from_str("2020-01-01 02:00:00", "%Y-%m-%d %H:%M:%S").unwrap(),
+            (0..24).map(|i| i as f32).collect(),
+            1, // 2. hour
+        )
+    }
+
+    #[test]
+    fn test_price() {
+        let ctx = setup();
+
+        let condition = Condition::PriceLowerThan(100.0);
+        assert!(condition.evaluate(&ctx));
+
+        let condition = Condition::PriceLowerThan(0.0);
+        assert!(!condition.evaluate(&ctx));
+    }
+
+    #[test]
+    fn test_hours() {
+        let ctx = setup();
+
+        assert!(Condition::Hours(0, 2).evaluate(&ctx));
+        assert!(!Condition::Hours(3, 4).evaluate(&ctx));
+        assert!(Condition::Hours(1, 3).evaluate(&ctx));
+    }
+
+    #[test]
+    fn test_percentile() {
+        let ctx = setup();
+
+        let result = Condition::PercentileInRange {
+            value: 0.05,
+            range: Range::Today, // calculated percentil is 0.04347826
+        }
+        .evaluate(&ctx);
+        assert_eq!(result, true);
+
+        let result = Condition::PercentileInRange {
+            value: 0.04,
+            range: Range::Today, // calculated percentil is 0.04347826
+        }
+        .evaluate(&ctx);
+        assert_eq!(result, false);
+
+        let result = Condition::PercentileInRange {
+            value: 0.0,
+            range: Range::Future,
+        }
+        .evaluate(&ctx);
+        assert_eq!(
+            result, true,
+            "this is cheapiest one price in now and future"
+        );
+
+        let result = Condition::PercentileInRange {
+            value: 0.51,
+            range: Range::PlusMinusHours(1),
+        }
+        .evaluate(&ctx);
+        assert_eq!(result, true);
+
+        let result = Condition::PercentileInRange {
+            value: 0.49,
+            range: Range::PlusMinusHours(1),
+        }
+        .evaluate(&ctx);
+        assert_eq!(result, false);
+    }
+
+    #[test]
+    fn test_not() {
+        let ctx = setup();
+
+        assert_eq!(
+            Condition::Not(Box::new(Condition::Debug(true))).evaluate(&ctx),
+            false
+        );
+        assert_eq!(
+            Condition::Not(Box::new(Condition::Debug(false))).evaluate(&ctx),
+            true
+        );
+    }
+
+    #[test]
+    fn test_and() {
+        let ctx = setup();
+
+        // Empty is false
+        assert_eq!(Condition::And(vec![]).evaluate(&ctx), false);
+
+        // Single value have same result
+        assert_eq!(
+            Condition::And(vec![Condition::Debug(true)]).evaluate(&ctx),
+            true
+        );
+        assert_eq!(
+            Condition::And(vec![Condition::Debug(false)]).evaluate(&ctx),
+            false
+        );
+
+        // Combination table
+        assert_eq!(
+            Condition::And(vec![Condition::Debug(true), Condition::Debug(true)]).evaluate(&ctx),
+            true
+        );
+        assert_eq!(
+            Condition::And(vec![Condition::Debug(true), Condition::Debug(false)]).evaluate(&ctx),
+            false
+        );
+        assert_eq!(
+            Condition::And(vec![Condition::Debug(false), Condition::Debug(true)]).evaluate(&ctx),
+            false
+        );
+        assert_eq!(
+            Condition::And(vec![Condition::Debug(false), Condition::Debug(false)]).evaluate(&ctx),
+            false
+        );
+    }
+
+    #[test]
+    fn test_or() {
+        let ctx = setup();
+
+        // Empty is false
+        assert_eq!(Condition::Or(vec![]).evaluate(&ctx), false);
+
+        // Single value have same result
+        assert_eq!(
+            Condition::Or(vec![Condition::Debug(true)]).evaluate(&ctx),
+            true
+        );
+        assert_eq!(
+            Condition::Or(vec![Condition::Debug(false)]).evaluate(&ctx),
+            false
+        );
+
+        // Combination table
+        assert_eq!(
+            Condition::Or(vec![Condition::Debug(true), Condition::Debug(true)]).evaluate(&ctx),
+            true
+        );
+        assert_eq!(
+            Condition::Or(vec![Condition::Debug(true), Condition::Debug(false)]).evaluate(&ctx),
+            true
+        );
+        assert_eq!(
+            Condition::Or(vec![Condition::Debug(false), Condition::Debug(true)]).evaluate(&ctx),
+            true
+        );
+        assert_eq!(
+            Condition::Or(vec![Condition::Debug(false), Condition::Debug(false)]).evaluate(&ctx),
+            false
+        );
     }
 }
 
@@ -239,6 +414,16 @@ mod prices_context_test {
         };
 
         assert_eq!(range.percentile(), 1.0);
+    }
+
+    #[test]
+    fn test_percentile_random() {
+        let range = PricesContext {
+            now_index: 1,
+            prices: vec![1.0, 2.0, 3.0, 4.0, 5.0],
+        };
+
+        assert_eq!(range.percentile(), 0.25);
     }
 
     #[test]
